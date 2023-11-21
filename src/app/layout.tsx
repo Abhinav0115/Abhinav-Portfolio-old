8414c72dc0a4b3045fdfa46f9749724cdcbed283
Abhinav Anand <Abhinav011501@gmail.com>
2023-11-22 00:22:38 +0530

    minor: change in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>
+            <body className="select-none">
                 <MenuProvider>
                     <ContactFormProvider>
                         <ContactForm />
